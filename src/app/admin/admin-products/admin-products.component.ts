561c79920772c7bb43bbad7a0987ae9e47d23873
stizzle123 <arifayanidowu2@yahoo.com>
2019-05-14 13:06:51 +0100

    Search filter logic created

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,24 +1,41 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { ProductService } from "src/app/services/product.service";
 import { map } from "rxjs/operators";
+import { Subscription } from "rxjs";
+import { Product } from "src/app/models/product";
 
 @Component({
   selector: "app-admin-products",
   templateUrl: "./admin-products.component.html",
   styleUrls: ["./admin-products.component.scss"]
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: Product[];
+  filteredProducts: any[];
+  subscription: Subscription;
   constructor(private productService: ProductService) {}
 
   ngOnInit() {
-    this.products$ = this.productService
+    this.subscription = this.productService
       .getAll()
       .snapshotChanges()
       .pipe(
         map(changes =>
           changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
         )
-      );
+      )
+      .subscribe(p => (this.filteredProducts = this.products = p));
+  }
+
+  filter(query: string) {
+    this.filteredProducts = query
+      ? this.products.filter(p =>
+          p.title.toLowerCase().includes(query.toLowerCase())
+        )
+      : this.products;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -15,6 +15,7 @@ import { Product } from "src/app/models/product";
 export class ProductFormComponent implements OnInit {
   categories$;
   product = {};
+  id: any;
 
   constructor(
     private router: Router,
@@ -25,16 +26,23 @@ export class ProductFormComponent implements OnInit {
 
   ngOnInit() {
     this.categories$ = this.categoryService.getCategories().valueChanges();
-    const id = this.route.snapshot.paramMap.get("id");
-    if (id)
+    this.id = this.route.snapshot.paramMap.get("id");
+    if (this.id)
       this.productService
-        .get(id)
+        .get(this.id)
         .valueChanges()
         .pipe(take(1))
         .subscribe(p => (this.product = p));
   }
   save(product) {
-    this.productService.create(product);
+    if (this.id) this.productService.update(this.id, product);
+    else this.productService.create(product);
+    this.router.navigate(["/admin/products"]);
+  }
+
+  delete() {
+    if (!confirm("Are you sure you want to delete this product?")) return;
+    this.productService.delete(this.id);
     this.router.navigate(["/admin/products"]);
   }
 }
