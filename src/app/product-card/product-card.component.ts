108e9cd6eed661ca51498ce1569bfb1a320a39ee
stizzle123 <arifayanidowu2@yahoo.com>
2019-05-15 23:06:09 +0100

    Making re-usable components

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Product } from "src/app/models/product";
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-product-card',
-  templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.scss']
+  selector: "product-card",
+  templateUrl: "./product-card.component.html",
+  styleUrls: ["./product-card.component.scss"]
 })
 export class ProductCardComponent implements OnInit {
+  @Input("product") product: Product;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
