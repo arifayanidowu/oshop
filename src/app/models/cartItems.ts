49779aea239c9f2af9145814cbe45b44d77aa6f2
stizzle123 <arifayanidowu2@yahoo.com>
2019-05-17 22:53:21 +0100

    Cleaning cart

--- src/app/models/cartItems.ts
+++ src/app/models/cartItems.ts
@@ -1,11 +1,20 @@
 import { Cart } from "./cart";
 export class CartItem {
-  constructor(public items: Cart[]) {}
+  items: Cart[] = [];
+  constructor(public itemsMap: { [productId: string]: Cart }) {
+    for (let productId in this.itemsMap) {
+      this.items.push(itemsMap[productId]);
+    }
+  }
+
+  get productIds() {
+    return Object.keys(this.itemsMap);
+  }
 
   get totalItemsCount() {
     let count = 0;
-    for (let productId in this.items) {
-      count += this.items[productId].quantity;
+    for (let productId in this.itemsMap) {
+      count += this.itemsMap[productId].quantity;
     }
     return count;
   }
--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from "./../services/shopping-cart.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-shopping-cart',
-  templateUrl: './shopping-cart.component.html',
-  styleUrls: ['./shopping-cart.component.scss']
+  selector: "app-shopping-cart",
+  templateUrl: "./shopping-cart.component.html",
+  styleUrls: ["./shopping-cart.component.scss"]
 })
 export class ShoppingCartComponent implements OnInit {
+  cart$;
 
-  constructor() { }
+  constructor(private shoppingCartService: ShoppingCartService) {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.cart$ = await this.shoppingCartService.getCart();
   }
-
 }
